ca0675f3c57474bc8a49aa2eb903cb6f63ace298
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-02 17:05:52 -0700

    css

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,24 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react';
+import '../css/app.css';
 
+class Header extends Component {
+	render() {
+		return (
+			<div className={'app-header'}>
+				<div className={'app-site-info'}>
+					<h1 className="name">Dary Barclay</h1>
+					<div className="name" className={'site-title'}>
+						Professional{' '}
+					</div>
+					<div className="name" className={'site-title'}>
+						File Sharing
+					</div>
 
-
-class Header extends Component{
-
-    render(){
-        return(<div className={'app-header'}>
-           <div className={'app-site-info'}>
-               <h1>SHARE</h1>
-               <div className={'site-title'}>Share Your Files.</div>
-               <div className={'site-slogan'}>Secure. Safe. Free.</div>
-
-           </div>
-        </div>)
-    }
+					<div className={'site-slogan'}>Secure. Safe. Free.</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Header;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,105 +1,87 @@
-import React,{Component} from 'react'
-import Header from '../components/header'
-import HomeForm from "../components/home-form"
-import HomeUploading from "../components/home-uploading"
-import HomeUploadSent from "../components/home-upload-sent"
+import React, { Component } from 'react';
+import Header from '../components/header';
+import HomeForm from '../components/home-form';
+import HomeUploading from '../components/home-uploading';
+import HomeUploadSent from '../components/home-upload-sent';
 import _ from 'lodash';
 
+class Home extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			componentName: 'HomeForm',
+			data: null,
+			uploadEvent: null
+		};
 
- class Home extends Component{
+		this._renderComponent = this._renderComponent.bind(this);
+	}
 
-    constructor(props){
-        super(props);
+	_renderComponent() {
+		const { componentName, data, uploadEvent } = this.state;
 
-        this.state = {
-            componentName: 'HomeForm',
-            data: null, 
-            uploadEvent: null, 
-        }
+		switch (componentName) {
+			case 'HomeUploading':
+				return (
+					<HomeUploading
+						onCancel={() => {
+							this.setState({
+								uploadEvent: null,
+								data: null,
+								componentName: 'HomeForm'
+							});
+						}}
+						event={uploadEvent}
+						data={data}
+					/>
+				);
 
-        this._renderComponent = this._renderComponent.bind(this)
-    }
+			case 'HomeUploadSent':
+				return (
+					<HomeUploadSent
+						onSendAnotherFile={() => {
+							this.setState({
+								componentName: 'HomeForm'
+							});
+						}}
+						data={data}
+					/>
+				);
 
-    _renderComponent(){
-
-        const {componentName, data, uploadEvent} = this.state;
-
-        switch (componentName){
-
-            case 'HomeUploading':
-
-            return <HomeUploading onCancel={() => {
-
-                this.setState({
-                    uploadEvent: null, 
-                    data: null, 
-                    componentName: 'HomeForm'
-
-                })
-
-            }} event={uploadEvent}  data={data}/>
-
-            case 'HomeUploadSent':
-
-            return (
-                <HomeUploadSent onSendAnotherFile={() => {
-
-                    this.setState({
-                        componentName: 'HomeForm'
-                    })
-
-                }} data={data} />
-            );
-
-            default: 
-                return  <HomeForm 
-                onUploadEvent={(event) =>{
-
-                    let data = this.state.data;
-                    if(_.get(event, 'type') === 'success'){
-
-                        data = _.get(event, 'payload');
-
-                    }
-                    this.setState(
-                        {
-                            data: data, 
-                            uploadEvent: event,
-                            componentName: (_.get(event, 'type') === 'success') ? 'HomeUploadSent': this.state.componentName,
-                        }
-                        );
-
-
-                }}
-                onUploadBegin={(data) => {
-                        
-                this.setState({
-                    data: data,
-                    componentName: 'HomeUploading',
-                });
-            }}
-                
-                />
-                return
-
-        }
-
-    }
-    render(){
-
-        return (
-            <div className={'app-container'}>
-            <Header/>
-                    <div className={'app-content'}>
-
-                        {this._renderComponent()}
-                       
-                    
-                        </div>
-                    </div>
-        )
-    }
+			default:
+				return (
+					<HomeForm
+						onUploadEvent={(event) => {
+							let data = this.state.data;
+							if (_.get(event, 'type') === 'success') {
+								data = _.get(event, 'payload');
+							}
+							this.setState({
+								data: data,
+								uploadEvent: event,
+								componentName:
+									_.get(event, 'type') === 'success' ? 'HomeUploadSent' : this.state.componentName
+							});
+						}}
+						onUploadBegin={(data) => {
+							this.setState({
+								data: data,
+								componentName: 'HomeUploading'
+							});
+						}}
+					/>
+				);
+		}
+	}
+	render() {
+		return (
+			<div className={'app-container'}>
+				<Header />
+				<div className={'app-content'}>{this._renderComponent()}</div>
+			</div>
+		);
+	}
 }
 
 export default Home;
